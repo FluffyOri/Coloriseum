c888b1b78c00be472a21620c40eeefaef21a31e8
Maxezify <gillet.maxence.isart@gmail.com>
2014-01-25 05:17:09 +0100

    First Menu

---
+++ code/join.js
@@ -0,0 +1,21 @@
+/*varButtJoin = document.getElementById('ButtonJoin');*/
+var ButtJoin = true;
+
+
+function join(){
+						if (ButtJoin === true) {
+
+								$('#ButtonJoin').animate({ x: '100px' });
+							    $('#ButtonJoin').animate({ opacity: '1' }, { duration:400, queue:false });
+								ButtJoin = false;
+								}
+								
+								else if (ButtJoin === false) {
+
+								$('#ButtonJoin').animate({ x: '-25px' });
+							   	 $('#ButtonJoin').animate({ opacity: '0' }, { duration:400, queue:false });
+                                ButtJoin = true;
+
+								}
+							}
+							 
