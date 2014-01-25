6c406a9fa7fa4495a6118606561f4896f54afb52
Maxezify <gillet.maxence.isart@gmail.com>
2014-01-26 00:08:05 +0100

    Update Menu + Credits final
    Page de crédits fini, et menu quasiment fini

--- menu/code/join.js
+++ menu/code/join.js
@@ -19,10 +19,25 @@ function join(){
 
 function nom()  {
 
-$('.LesNoms').animate({ y: '1400px' },  { duration: 35000 });
+$('.LesNoms').animate({ y: '1300px' },  { duration: 30000 });
 
 }
 
+function Pop()   {
+
+								$('#ButtonStart').delay(2000);
+								$('#ButtonStart').transition({ scale: 1.2 });
+								$('#ButtonStart').transition({ scale:0.9} );
+								$('#ButtonStart').delay(10);
+								$('#ButtonStart').transition({ scale:1.0} );
+								setTimeout("Pop()",750);
+
+
+
+}
+
+
 $(document).ready(function() {
       nom();
+      Pop();
 });
--- menu/code/location.js
+++ menu/code/location.js
@@ -4,20 +4,9 @@ window.location="start.html";
 
 }
 
-function Settingsloc()  {
-
-window.location="settings.html";
-
-}
 
 function Creditsloc()  {
 
 window.location="credits.html";
-
-}
-
-function Createloc()   {
-
-window.location="create.html";
 
 }
